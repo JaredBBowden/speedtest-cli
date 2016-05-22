ce71d4ccce92ce832e4524b1ea9e67592da2b8da
Jared Bowden <jaredbbowden@gmail.com>
2016-05-22 15:45:01 -0500

    Adds TODO planning for future functionality

--- graph_speed.py
+++ graph_speed.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pylab as plt
 
+# TODO let's update the way that we're parsing time here
 dateparse = lambda x: pd.datetime.strptime(x, '%Y/%d/%m %H:%M:%S')
 
 raw_data = pd.read_csv("./speed_frame.csv", parse_dates=['Time'], date_parser=dateparse)
@@ -27,10 +28,14 @@ raw_data["Time"] = pd.to_datetime(raw_data["Time"])
 
 plt.scatter(range(len(raw_data["Time"])), raw_data["Download"], color = raw_data["temp_color"], alpha = 0.8)
 
-#TODO add units and parse time correctly
 plt.title("Moontower Internet Speed")
 plt.xlabel("Time")
 plt.ylabel("Speed")
 
 plt.show()
 plt.close()
+
+# TODO add a histogram of speed. Would like to do things in a way that
+# makes a distinction between VPN and Time Warner.
+
+# TODO add some weekly and monthly summaries
